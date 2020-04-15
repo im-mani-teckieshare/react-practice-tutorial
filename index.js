7d381ab8abcd685800ff647a44568f451add35c3
im-mani-teckieshare <teckieshare@gmail.com>
2020-04-15 20:02:05 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./style.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <h2>Basic JSX tutorial</h2>
+  </div>,
+  document.getElementById("root")
+);
