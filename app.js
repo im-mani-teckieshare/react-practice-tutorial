91bcb22a3460836e57e0529f345c76647d83ff6f
im-mani-teckieshare <teckieshare@gmail.com>
2020-04-15 21:56:38 +0530

    1 file modified, 1 added

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React from "react";
+import HelloWorld from "./hello-world";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <span>Hello World</span>
+      <HelloWorld />
+      <h1>User</h1>
+    </div>
+  );
 }
 
 export default App;
