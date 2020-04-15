c9145b82aaa0ed8457a3d9c5ae6db894d767b697
im-mani-teckieshare <teckieshare@gmail.com>
2020-04-15 21:30:24 +0530

    1 file modified

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React from "react";
+
+const name = "Teckieshare";
+
+function displayElement(){
+  return <h1>Its come from Function</h1>
+}
+
+function showFullUser(user){
+return user.firstName+' '+user.lastName;
+}
+
+const user={
+  firstName: 'Jhon',
+  lastName: 'David'
+}
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div>Hello World </div>
+      <h1>{name}</h1>
+      {displayElement()}
+      <span>{2+2}</span>
+      <h2>{showFullUser(user)}</h2>
+    </div>
+  );
 }
 
 export default App;
