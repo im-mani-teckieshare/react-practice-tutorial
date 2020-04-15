3418b12a5edbc432739465d2585ae1982ed4f36e
im-mani-teckieshare <teckieshare@gmail.com>
2020-04-15 21:40:08 +0530

    2 files modified, 1 added

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./app.css";
+
+const divStyle = {
+  color: "blue",
+  fontSize: "24px"
+};
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div style={{ color: "pink" }}>Inline style</div>
+      <div style={divStyle}>From expression Style</div>
+      <div>Normal CSS Styling</div>
+      <div className="global-div">Global CSS Styling</div>
+    </div>
+  );
 }
 
 export default App;
