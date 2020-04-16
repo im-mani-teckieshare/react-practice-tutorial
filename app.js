287122d96ab57e8af3ced4c3590141ef93c482cd
im-mani-teckieshare <teckieshare@gmail.com>
2020-04-16 07:52:38 +0530

    2 files modified

--- app.js
+++ app.js
@@ -2,27 +2,32 @@ import React from "react";
 
 const name = "Teckieshare";
 
-function displayElement(){
-  return <h1>Its come from Function</h1>
-}
-
-function showFullUser(inputObj){
-return inputObj.firstName+' '+inputObj.lastName;
-}
-
-const user={
-  firstName: 'Jhon',
-  lastName: 'David'
-}
 
 function App() {
+function clickEvent() {
+  alert("Click Event Fired");
+}
+
+function changeEvent(e){
+ console.log(e.target.value)
+}
+
   return (
     <div>
       <div>Hello World </div>
       <h1>{name}</h1>
-      {displayElement()}
-      <span>{2+2}</span>
-      <h2>{showFullUser(user)}</h2>
+      {/**<button onclick="clickEvent()">
+  Click Me
+</button> */}
+      {/*<button onClick={clickEvent}>Click Me</button>*/}
+      {/* <button onClick={clickEvent()}>Without Click I will fire</button> */}
+      <button onClick={clickEvent}>Now Click I will fire</button>
+      <hr />
+      <button onClick={() => clickEvent()}>Inline Event</button>
+      <hr/>
+      <input onChange={changeEvent}/>
+      <h2>Inline Change Event</h2>
+      <input onChange={(e)=>changeEvent(e)}/>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -3,20 +3,4 @@ import ReactDOM from "react-dom";
 import "./style.css";
 import App from './app';
 
-// ReactDOM.render(
-//   <div>
-//     Hello World
-//   </div>,
-//   document.getElementById("root")
-// );
-
-// ReactDOM.render(
-//   React.createElement("div", null, ["Hello World"]),
-//   document.getElementById("root")
-// );
-
-// function App() {
-//   return <div>Hello World</div>;
-// }
-
 ReactDOM.render(<App />, document.getElementById("root"));
