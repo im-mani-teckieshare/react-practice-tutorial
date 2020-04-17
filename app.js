eb1b4b1f39971a3b3641cecfefef3e08297748a1
im-mani-teckieshare <teckieshare@gmail.com>
2020-04-17 21:21:35 +0530

    2 files modified

--- app.js
+++ app.js
@@ -2,31 +2,39 @@ import React from "react";
 import User from "./user";
 import CustomerList from "./customerList";
 
-function App() {
-  const user = {
-    firstName: "Rakul",
-    lastName: "kumar"
+// function App() {
+//   return (
+//     <div>
+//       <h3>TeckieShare</h3>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  user = {
+    firstName: "John",
+    lastName: "David"
   };
-  const customer = ['Ravi','John','David']
-  return (
-    // <div>
-    //   <h1>Parent Component</h1>
-    // </div>
-    // <div>
-    //   <h1>Parent Component</h1>
-    //   <User />
-    // </div>
-    // <div>
-    //   <h1>Parent Component</h1>
-    //   <User userName="Rakul"/>
-    // </div>
-    <div>
-      <h1>Parent Component</h1>
-      {/* <User firstName="Rakul" lastName="Kumar"/> */}
-      <User {...user} />
-      <CustomerList customer={customer} />
-    </div>
-  );
+
+  userName = "Teckieshare";
+
+  clickEvent() {
+    alert("click Event Fired");
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <h3>TeckieShare</h3> */}
+        <h3>{this.userName}</h3>
+        <button onClick={this.clickEvent}>Click Me</button>
+        <hr />
+        <h4>Props in Class Component</h4>
+        <br />
+        <User {...this.user} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- user.js
+++ user.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 
-function User(props) {
-  return (
-    // <div>
-    //   <h2>User Component</h2>
-    // </div>
-    // <div>
-    //   <h2>Hello, {userName}</h2>
-    // </div>
-    <div>
-      {/* <h2>Hello, {props.userName}</h2> */}
-     <h2>Hello, {props.firstName} {props.lastName}</h2>
-    </div>
-  );
+// function User(props) {
+//   return (
+//     <div>
+//       <h2>
+//         Hello, {props.firstName} {props.lastName}
+//       </h2>
+//     </div>
+//   );
+// }
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          Hello, {this.props.firstName} {this.props.lastName}
+        </h2>
+      </div>
+    );
+  }
 }
-
 export default User;
