8f7f5841f845b2ba7d1a091530d05c8730984dbc
im-mani-teckieshare <teckieshare@gmail.com>
2020-04-18 12:00:55 +0530

    1 file modified

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import User from "./user";
-import Axios from 'axios'
+import Axios from "axios";
 
 class App extends React.Component {
-
   state = {
-    user:[]
-  }
+    user: []
+  };
 
   componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  // .then(response => response.json())
-  // .then(json => this.setState({'user':json}))
-    Axios.get('https://jsonplaceholder.typicode.com/users')
-  .then(({data}) => this.setState({'user':data}))
+    //   fetch('https://jsonplaceholder.typicode.com/users')
+    // .then(response => response.json())
+    // .then(json => this.setState({'user':json}))
+    Axios.get("https://jsonplaceholder.typicode.com/users").then(({ data }) =>
+      this.setState({ user: data })
+    );
   }
 
-  showUser(){
-    return this.state.user.map(e => <div>{e.name} - {e.email}</div>)
+  showUser() {
+    return this.state.user.map(e => (
+      <div>
+        {e.name} - {e.email}
+      </div>
+    ));
   }
 
   render() {
-    return (
-      <div>
-      {this.showUser()}
-      </div>
-    );
+    return <div>{this.showUser()}</div>;
   }
 }
 
